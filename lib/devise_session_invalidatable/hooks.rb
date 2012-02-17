c405c91516d93846fb8492de4d78397eae9083c0
thickpaddy <mwoods@online.ie>
2012-02-17 16:46:11 +0000

    Fix bug, it actually works now

--- lib/devise_session_invalidatable/hooks.rb
+++ lib/devise_session_invalidatable/hooks.rb
@@ -8,7 +8,7 @@ end
 # Check for a valid session when fetching the record from the session
 Warden::Manager.after_set_user :only => :fetch do |record, warden, options|
   scope = options[:scope]
-  if warden.authenticated?(scope) && record.invalidated?
+  if warden.authenticated?(scope) && record.session_invalidated?
     path_checker = Devise::PathChecker.new(warden.env, scope)
     unless path_checker.signing_out?
       throw :warden, :scope => scope, :message => "Session no longer valid. Please sign in."
