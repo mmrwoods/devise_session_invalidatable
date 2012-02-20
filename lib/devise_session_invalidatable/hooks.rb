c741f5c07537a1b9222488da61ce79e1c942cc92
thickpaddy <mwoods@online.ie>
2012-02-20 12:34:18 +0000

    Avoid blowing the stack

--- lib/devise_session_invalidatable/hooks.rb
+++ lib/devise_session_invalidatable/hooks.rb
@@ -1,17 +1,14 @@
 # Invalidate the session on logout (i.e. clear the current sign in timestamp and ip)
 Warden::Manager.before_logout do |record, warden, options|
-  if warden.authenticated?(options[:scope])
-    record.invalidate_session!
-  end
+  record.invalidate_session! if record
 end
 
 # Check for a valid session when fetching the record from the session
 Warden::Manager.after_set_user :only => :fetch do |record, warden, options|
   scope = options[:scope]
-  if warden.authenticated?(scope) && record.session_invalidated?
-    path_checker = Devise::PathChecker.new(warden.env, scope)
-    unless path_checker.signing_out?
-      throw :warden, :scope => scope, :message => "Session no longer valid. Please sign in."
-    end
+  path_checker = Devise::PathChecker.new(warden.env, scope)
+  if !path_checker.signing_out? && record && record.session_invalidated?
+    warden.logout(scope)
+    throw :warden, :scope => scope, :message => "Session no longer valid. Please sign in."
   end
 end
